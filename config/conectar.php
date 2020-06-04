ea0d3c7ab7b650d0cdb2d713d22c76d3558d130e
Inventory System Web <ramirezw65@gmail.com>
2020-06-04 04:21:03 -0500

    cambiamos parametros de conexion a base de datos de Local a Web.

--- config/conectar.php
+++ config/conectar.php
@@ -5,11 +5,13 @@ class conectarr{
 
 
 		try{
-	$conexion=new PDO('mysql:host=localhost; dbname=sistemweb','root','');
+	$conexion=new PDO('mysql:host=us-cdbr-east-05.cleardb.net; dbname=heroku_1f5ff059f1d7813','b99c4da36713e8','362d39c9');
 
 	$conexion->setattribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	$conexion->exec("SET CHARACTER SET UTF8");
+
+	//mysql://b99c4da36713e8:362d39c9@us-cdbr-east-05.cleardb.net/heroku_1f5ff059f1d7813?reconnect=true
 
 
 
--- config/database.php
+++ config/database.php
@@ -4,8 +4,10 @@
 		
 		public static function conexion(){
 			
-			$conexion = new mysqli("localhost", "root", "", "sistemweb");
+			$conexion = new mysqli("us-cdbr-east-05.cleardb.net", "b99c4da36713e8", "362d39c9", "heroku_1f5ff059f1d7813");
 			return $conexion;
+
+			//mysql://b99c4da36713e8:362d39c9@us-cdbr-east-05.cleardb.net/heroku_1f5ff059f1d7813?reconnect=true
 			
 		}
 	}
