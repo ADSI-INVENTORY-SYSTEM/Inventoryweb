f4aa4e1ed85cc85b2915abc10f3b0dc54b2fba6a
Inventory System Web <ramirezw65@gmail.com>
2020-06-08 10:14:09 -0500

    se interfiere en ruta de imagen

--- views/activos/activos_modifica.php
+++ views/activos/activos_modifica.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Nuevo Activo</title>
+		<title>Modificar Activo</title>
 		<link rel="stylesheet" type="text/css" href="estilodelistas.css">
 		<?php include 'scripts.php'; ?>
 		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
