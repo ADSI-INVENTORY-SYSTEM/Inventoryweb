82a7b83fe50bb2d02c874a484eabdfc6b46b4949
Inventory System Web <ramirezw65@gmail.com>
2020-06-17 17:53:02 -0500

    arreglo contra

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
                 <a href="index1.php?c=Usuarios&a=nuevoAprendiz" id="SingIn" class="mdl-button mdl-js-button mdl-js-ripple-effect" style="color: #3F51B5; float:left;">Registrarme</a>
             </div>
 			<div class="d-flex justify-content-center links">
-                  <a href="recuperacontra/recupera.php">Olvidaste Tu Contraseña?</a>
+                  <a href="recupera.php">Olvidaste Tu Contraseña?</a>
             </div>
 		</form>
 	</div>
