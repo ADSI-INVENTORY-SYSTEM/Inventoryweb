2037979c95f6af9bb88ff37c26836be836dac565
Inventory System Web <ramirezw65@gmail.com>
2020-06-21 20:04:20 -0500

    se añade conteo de activos en pagina principal

--- home.php
+++ home.php
@@ -30,66 +30,108 @@
 		<section class="full-width text-center" style="padding: 40px 0;">
 			<h3 class="text-center tittles">CATEGORIAS</h3>
 			<!-- Tiles -->
+			<?php
+				require_once "ConexionDatos.php";
+				$conex     = new conexiondatos();
+				$con1      = $conex->conectar();
+				$resultado = mysqli_query($con1, "SELECT COUNT(Categoria_idcategoria) as mtotal from activos where Categoria_idcategoria = 1 AND Estado_idEstado = 1 ");
+				$resultado1 = mysqli_fetch_array($resultado); 
+			?>
 			<article class="full-width tile">
 				<a href="Monitoresindex.php">
 					<div class="tile-text">
 					<span class="text-condensedLight">
-						<br>
+						<?php echo $resultado1["mtotal"]; ?><br>
 						<small>Monitores</small>
 					</span>
 				</div>
 				<i class="zmdi zmdi-desktop-windows tile-icon"></i>		
 				</a>
 			</article>
+			<?php
+				require_once "ConexionDatos.php";
+				$conex     = new conexiondatos();
+				$con1      = $conex->conectar();
+				$resultado = mysqli_query($con1, "SELECT COUNT(Categoria_idcategoria) as Ttotal from activos where Categoria_idcategoria = 2 AND Estado_idEstado = 1  ");
+				$resultado1 = mysqli_fetch_array($resultado); 
+			?>
 			<article class="full-width tile">
 				<a href="Torresindex.php">
 				<div class="tile-text">
 					<span class="text-condensedLight">
-						<br>
+						<?php echo $resultado1["Ttotal"]; ?><br>
 						<small>Torres</small>
 					</span>
 				</div>
 				<i class="zmdi zmdi-speaker tile-icon"></i>
 				</a>
 			</article>
+			<?php
+				require_once "ConexionDatos.php";
+				$conex     = new conexiondatos();
+				$con1      = $conex->conectar();
+				$resultado = mysqli_query($con1, "SELECT COUNT(Categoria_idcategoria) as Mtotal from activos where Categoria_idcategoria = 3 AND Estado_idEstado = 1  ");
+				$resultado1 = mysqli_fetch_array($resultado); 
+			?>
 			<article class="full-width tile">
 				<a href="Mousesindex.php">
 				<div class="tile-text">
 					<span class="text-condensedLight">
-						<br>
+						<?php echo $resultado1["Mtotal"]; ?><br>
 						<small>Mouses</small>
 					</span>
 				</div>
 				<i class="zmdi zmdi-mouse tile-icon"></i>
 				</a>
 			</article>
+			<?php
+				require_once "ConexionDatos.php";
+				$conex     = new conexiondatos();
+				$con1      = $conex->conectar();
+				$resultado = mysqli_query($con1, "SELECT COUNT(Categoria_idcategoria) as ttotal from activos where Categoria_idcategoria = 4 AND Estado_idEstado = 1  ");
+				$resultado1 = mysqli_fetch_array($resultado); 
+			?>
 			<article class="full-width tile">
 				<a href="Tecladosindex.php">
 				<div class="tile-text">
 					<span class="text-condensedLight">
-						<br>
+						<?php echo $resultado1["ttotal"]; ?><br>
 						<small>Teclados</small>
 					</span>
 				</div>
 				<i class="zmdi zmdi-keyboard tile-icon"></i>
 				</a>
 			</article>
+			<?php
+				require_once "ConexionDatos.php";
+				$conex     = new conexiondatos();
+				$con1      = $conex->conectar();
+				$resultado = mysqli_query($con1, "SELECT COUNT(Categoria_idcategoria) as Ctotal from activos where Categoria_idcategoria = 5 AND Estado_idEstado = 1  ");
+				$resultado1 = mysqli_fetch_array($resultado); 
+			?>
 			<article class="full-width tile">
 				<a href="Cableadoindex.php">
 				<div class="tile-text">
 					<span class="text-condensedLight">
-						<br>
+						<?php echo $resultado1["Ctotal"]; ?><br>
 						<small>Cableado</small>
 					</span>
 				</div>
 				<i class="zmdi zmdi-router tile-icon"></i>
 				</a>
 			</article>
+			<?php
+				require_once "ConexionDatos.php";
+				$conex     = new conexiondatos();
+				$con1      = $conex->conectar();
+				$resultado = mysqli_query($con1, "SELECT COUNT(Categoria_idcategoria) as Atotal from activos where Categoria_idcategoria = 6 AND Estado_idEstado = 1  ");
+				$resultado1 = mysqli_fetch_array($resultado); 
+			?>
 			<article class="full-width tile" >
 				<a href="Accesorioindex.php">
 				<div class="tile-text">
 					<span class="text-condensedLight">
-						<br>
+						<?php echo $resultado1["Atotal"]; ?><br>
 						<small >Accesorios</small>
 					</span>
 				</div>
